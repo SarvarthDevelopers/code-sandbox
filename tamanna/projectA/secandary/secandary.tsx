d7ee070897757ac9841accea8cdffb29931180e2
Tamanna320 <tamanna.thakur@sarvarth.com>
2025-07-08 14:13:25 +0530

    Add files via upload

---
+++ tamanna/projectA/secandary/secandary.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./secandary.css"
+import { GoArrowRight } from "react-icons/go";
+
+
+
+ export const Secandary:React.FC=()=>{
+return(
+    <>
+    <button className="Sbtn">Secandary CTA <GoArrowRight className="Sicon"/></button>
+   
+    </>
+)
+}
